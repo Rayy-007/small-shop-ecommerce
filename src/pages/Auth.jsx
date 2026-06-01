ed686c42c4cb7cda58a9e6d61c639abae39d16f6
Rayy-007 <minkhant88887@gmail.com>
2026-06-01 14:42:01 +0630

    added auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,16 +1,38 @@
-import { useState } from "react";
+import { use, useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
   const [mode, setMode] = useState("signup");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const { signUp, user, logout, login } = useContext(AuthContext);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  function onSubmit() {
-    alert("Sign Up");
+  function onSubmit(data) {
+    setError(null);
+
+    let result;
+    if (mode === "signup") {
+      result = signUp(data.email, data.password);
+    } else {
+      result = login(data.email, data.password);
+    }
+
+    if (!result.success) {
+      setError(result.error);
+    } else {
+      navigate("/");
+    }
+
+    console.log(result);
   }
 
   return (
@@ -25,6 +47,9 @@ export default function Auth() {
             className="auth-form"
             onSubmit={handleSubmit(onSubmit)}
           >
+            {error && <div className="error-message">{error}</div>}
+            <button onClick={() => logout()}>Logout</button>
+            {user && <p>User singed up - {user.email}</p>}
             <div className="form-group">
               <label htmlFor="email" className="form-label">
                 Email
